e4bb96b218440c4b9715ede20d332a6844c84e5d
Tall Paul <pmd6624@rit.edu>
2015-04-10 16:44:12 -0400

    added user organizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410204052) do
+ActiveRecord::Schema.define(version: 20150410204343) do
 
   create_table "community_services", force: :cascade do |t|
     t.integer  "event_id"
@@ -45,6 +45,18 @@ ActiveRecord::Schema.define(version: 20150410204052) do
   end
 
   add_index "philanthropies", ["event_id"], name: "index_philanthropies_on_event_id"
+
+  create_table "user_organizations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "organization_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "user_organizations", ["organization_id"], name: "index_user_organizations_on_organization_id"
+  add_index "user_organizations", ["user_id"], name: "index_user_organizations_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "login",               default: "", null: false
