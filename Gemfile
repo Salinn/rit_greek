d45f3c59ba9862714a31b04f7ebe0c9e7b1ff59c
salinn <pmd6624@rit.edu>
2015-04-09 21:30:31 -0400

    added basic user login  with ldap for users

--- Gemfile
+++ Gemfile
@@ -23,6 +23,10 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+#Creates user and allows connection to RIT's Server
+gem 'devise'
+gem 'devise_ldap_authenticatable'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  StaticPagesController.action_methods.each do |action|
+    get "/#{action}", to: "static_pages##{action}", as: "#{action}"
+  end
+
+  root :to => 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
