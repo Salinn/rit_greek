8a66358137c4fb03808ef340ed5b9fb8f6bd7502
Tall Paul <pmd6624@rit.edu>
2015-04-30 17:41:01 -0400

    can now delete users from organizations if not in that organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -6,12 +6,22 @@ class Organization < ActiveRecord::Base
 
   def self.import(file, organization)
     #http://richonrails.com/articles/importing-csv-files
+    user_ids = []
     CSV.foreach(file.path, headers: true) do |row|
 
       user_hash = row.to_hash # exclude the price field
       user = User.find_or_create_by(login: user_hash["login"])
 
-      UserOrganization.find_or_create_by(user: user, organization: organization)
+      user_ord_id = UserOrganization.find_or_create_by(user: user, organization: organization)
+      user_ids.push(user_ord_id.id)
     end # end CSV.foreach
+    users = UserOrganization.where(organization: organization)
+    users.each do |user|
+      if user_ids.include? user.id
+        user_ids.delete(user.id)
+      else
+        user.destroy
+      end
+    end
   end # end self.impo
 end
