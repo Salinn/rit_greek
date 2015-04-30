b8e4fb5e193ba5d5ab96a220045dac7bf21eeb43
Tall Paul <pmd6624@rit.edu>
2015-04-30 15:54:13 -0400

    fixed philantropies

--- app/helpers/community_services_helper.rb
+++ app/helpers/community_services_helper.rb
@@ -1,8 +1,11 @@
 module CommunityServicesHelper
   def organization_total_hours(organization)
     total = 0
-    organization.users do |user|
-      total +=  user.events.where(type_of_event: 'Community Service').total_hours
+    organization.users.each do |user|
+      test =  user.events.where(type_of_event: 'Community Service')
+      test.each do |event|
+        total += event.community_services.first.total_hours
+      end
     end
     total
   end
--- app/helpers/philanthropies_helper.rb
+++ app/helpers/philanthropies_helper.rb
@@ -1,8 +1,11 @@
 module PhilanthropiesHelper
   def organization_total_raised(organization)
     total = 0
-    organization.users do |user|
-      total +=  user.events.where(type_of_event: 'Philanthropy').total_raised
+    organization.users.each do |user|
+      test =  user.events.where(type_of_event: 'Philanthropy')
+      test.each do |event|
+        total += event.community_services.first.total_raised
+      end
     end
     total
   end
