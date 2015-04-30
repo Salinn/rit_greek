4cfc1b1ce3a4bbddaf583a3ac47375801e4a282e
Tall Paul <pmd6624@rit.edu>
2015-04-30 17:19:03 -0400

    Fixes #3

--- app/helpers/philanthropies_helper.rb
+++ app/helpers/philanthropies_helper.rb
@@ -1,11 +1,13 @@
 module PhilanthropiesHelper
   def organization_total_raised(organization)
     total = 0
+    seen = []
     organization.users.each do |user|
       test =  user.events.where(type_of_event: 'Philanthropy')
       test.each do |event|
-        if event.philanthropies.any?
+        if event.philanthropies.any? && !seen.include?(event.philanthropies.first.id)
           total += event.philanthropies.first.total_raised
+          seen.push(event.philanthropies.first.id)
         end
       end
     end
